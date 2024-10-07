af160857fb92cfa8fffbd0a3d024e0863fb2c935
hebraheem <iadebiyi01@gmail.com>
2024-10-08 01:19:05 +0200

    Added eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+// eslint-disable-next-line
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2024,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'no-console': 'warn',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+};
