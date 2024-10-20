f560d0fa5b5af7f48fcf4c4e003c5b85678df79d
hebraheem <iadebiyi01@gmail.com>
2024-10-20 21:40:05 +0200

    feat: add icons to auth-layout

--- src/layouts/auth-layout.tsx
+++ src/layouts/auth-layout.tsx
@@ -12,13 +12,21 @@ const AuthLayout = () => {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-custom-gradient">
-      <div className="relative sm:p-10 p-5 m-3 bg-white bg-opacity-20 backdrop-blur-md rounded-xl shadow-md w-full max-w-md border border-white border-opacity-30">
-        <div className="absolute inset-0 flex items-center justify-center z-0">
-          {/* Background Decoration */}
-          <div className="absolute top-0 left-0 w-16 h-16 bg-green-400 rounded-full"></div>
-          <div className="absolute top-24 right-0 w-24 h-24 bg-red-500 rounded-full"></div>
-          <div className="absolute bottom-20 left-12 w-32 h-32 bg-blue-400 rounded-full"></div>
+      <div className="absolute inset-10 flex items-center justify-center z-0">
+        <div className="absolute top-0 left-0 w-16 h-16 bg-green-400 rounded-full">
+          <img src="https://img.icons8.com/?size=100&id=PZpLpwIPUpaF&format=png&color=000000" alt="help-up" />
         </div>
+        <div className="absolute top-24 right-10 w-24 h-24 bg-red-500 rounded-full">
+          <img src="https://img.icons8.com/?size=100&id=23530&format=png&color=000000" alt="think" />
+        </div>
+        <div className="absolute bottom-20 left-12 w-32 h-32 bg-blue-400 rounded-full text-center">
+          <img src="https://img.icons8.com/?size=100&id=15975&format=png&color=000000" alt="hand-c" />
+        </div>
+        <div className="absolute bottom-72 right-72 w-32 h-32 bg-orange-400 rounded-full">
+          <img src="https://img.icons8.com/?size=100&id=p4rXi9HURgXT&format=png&color=000000" alt="help-logo" />
+        </div>
+      </div>
+      <div className="relative sm:p-10 p-5 m-3 bg-white bg-opacity-20 backdrop-blur-md rounded-xl shadow-md w-full max-w-md border border-white border-opacity-30">
         <div className="relative z-10">
           <Outlet />
         </div>
